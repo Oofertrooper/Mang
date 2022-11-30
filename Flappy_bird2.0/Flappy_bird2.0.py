d8430603aa46c6805cfecb5b8b508a76ca59522f
Oofertrooper <119326267+Oofertrooper@users.noreply.github.com>
2022-11-30 22:45:23 +0200

    Lisatudtaust

--- Flappy_bird2.0/Flappy_bird2.0.py
+++ Flappy_bird2.0/Flappy_bird2.0.py
@@ -3,11 +3,23 @@ import time
 import sys
 pygame.init()
 
-ekraan = pygame.display.set_mode([400, 600])
+pygame.display.set_caption("Flappy")
+icon = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Lind1.png")
+pygame.display.set_icon(icon)
 mäng_töötab = True
+
+
+ekraan = pygame.display.set_mode(([2000, 1000]), pygame.WINDOWMAXIMIZED)
 ekraan.fill([133, 193, 233])
-taust = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Taust_päev.png")
+taust = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Taust_õhtu.png")
+taust = pygame.transform.smoothscale(taust, ekraan.get_size())
 ekraan.blit(taust, (0, 0))
+
+lind1 = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Lind1.png")
+lind2 = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Lind2.png")
+lind1 = pygame.transform.scale(lind1, (300, 300))
+ekraan.blit(lind1, (500, 300))
+
 
 pygame.display.flip()
 while mäng_töötab:
