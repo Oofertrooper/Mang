5636f50278212668f4b0dc4989025da67db645d4
Oofertrooper <119326267+Oofertrooper@users.noreply.github.com>
2022-12-04 22:42:37 +0200

    pildid

--- Flappy_bird2.0/tempCodeRunnerFile.py
+++ Flappy_bird2.0/tempCodeRunnerFile.py
@@ -1,16 +1,13 @@
-import pygame
-import time
-import sys
-pygame.init()
-
-ekraan = pygame.display.set_mode([400, 600])
-mäng_töötab = True
-ekraan.fill([133, 193, 233])
-taust = pygame.image.load("taust_päev")
-
-
-pygame.display.flip()
-while mäng_töötab:
+dt = clock.tick() / 1000
+    nimi_y = (100)
+    
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             sys.exit()
+
+    nimi_y +=1
+
+    ekraan.blit(nimi, (700, nimi_y))
+    pygame.display.flip()
+
+    pygame.time.delay(17)
