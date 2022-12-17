6cc7fd2188a3f98057315a84dd3008951416667f
KrisuAru <119818146+KrisuAru@users.noreply.github.com>
2022-12-17 19:40:15 +0200

    parandasin pildi asukoha addressid

--- Flappy_bird2.0/Flappy_bird2.0.py
+++ Flappy_bird2.0/Flappy_bird2.0.py
@@ -5,24 +5,24 @@ pygame.init()
 clock = pygame.time.Clock()
 #Aken
 pygame.display.set_caption("Flappy")
-icon = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Lind1.png")
+icon = pygame.image.load("Assets\Lind1.png")
 pygame.display.set_icon(icon)
 mäng_töötab = True
 
 #Pildid
 ekraan = pygame.display.set_mode(([2000, 1000]), pygame.WINDOWMAXIMIZED)
 ekraan.fill([133, 193, 233])
-taust = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Taust_õhtu.png")
+taust = pygame.image.load("Assets\Taust_õhtu.png")
 taust = pygame.transform.smoothscale(taust, ekraan.get_size())
 ekraan.blit(taust, (0, 0))
 nimi_y = 300
-nimi = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Flappy_menu.png")
+nimi = pygame.image.load("Assets\Flappy_menu.png")
 nimi = pygame.transform.scale(nimi, (600, nimi_y))
 ekraan.blit(nimi, (700, 100))
 
 #Lind
-lind1 = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Lind1.png")
-lind2 = pygame.image.load("E:\Mäng\M-ng\Flappy_bird2.0\Assets\Lind2.png")
+lind1 = pygame.image.load("Assets\Lind1.png")
+lind2 = pygame.image.load("Assets\Lind2.png")
 lind1 = pygame.transform.scale(lind1, (300, 300))
 ekraan.blit(lind1, (500, 300))
 
